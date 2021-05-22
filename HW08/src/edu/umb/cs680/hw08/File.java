98a32cdfa2ac28950d2a8731934f1c68f20cfbd2
Robertkevin96 <64680746+Robertkevin96@users.noreply.github.com>
2021-05-22 01:28:48 -0400

    Add files via upload

---
+++ HW08/src/edu/umb/cs680/hw08/File.java
@@ -0,0 +1,22 @@
+package edu.umb.cs680.hw08;
+import java.time.LocalDateTime;
+
+public class File extends FSElement {
+	
+	public File(Directory main, String name, int size, LocalDateTime createdTime) {
+		super(main, name, size, createdTime);
+		main.appendChild(this);
+	}
+
+	public boolean isDirect() {
+		return false;
+	}
+	
+	public boolean isFile() {
+		return true;
+	}
+	
+	public boolean isLink() {
+		return false;
+	}
+}
---
+++ HW10/src/edu/umb/cs680/hw10/ApfsLink.java
@@ -0,0 +1,36 @@
+package edu.umb.cs680.hw10;
+
+import java.time.LocalDateTime;
+
+public class ApfsLink extends ApfsElement {
+	private ApfsElement target;
+
+	public ApfsLink(ApfsDirectory parent, String name, int size, LocalDateTime creationTime, String ownerName,
+			LocalDateTime lastModified, ApfsElement target) {
+		super(parent, name, size, creationTime, ownerName, lastModified);
+		
+		this.target = target;
+		parent.appendChild(this);
+	}
+
+	public ApfsElement getTarget() {
+		return target;
+	}
+
+	public void setTarget(ApfsElement target) {
+		this.target = target;
+	}
+
+	@Override
+	public boolean isDirectory() {
+		
+		return false;
+	}
+
+	@Override
+	public void accept(ApfsFSVisitor v) {
+	
+		v.visit(this);
+	}
+
+}
